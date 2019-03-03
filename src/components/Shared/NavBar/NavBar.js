17d4e3cc2058618bfd9a2bda05cf1c67ac5264d9
muhwezi deo <aggrey256@gmail.com>
2019-03-03 09:50:48 +0300

    feat(login): implement login for users - set up login components - set up actions and Reducer - update NavBar to show changes on login - add tests for functionality
    [Starts #164354861]

--- src/components/Shared/NavBar/NavBar.js
+++ src/components/Shared/NavBar/NavBar.js
@@ -10,18 +10,20 @@ const trigger = (
 )
 
 const options = [
-    { key: 'user', text: 'Profile', icon: 'user' },
-    { key: 'sign-out', text: 'Sign Out', icon: 'sign out' },
+    { key: 'user', text: 'Profile', icon: 'user', as:Link, to:'/login'},
+    { key: 'sign-out', text: 'Sign Out', icon: 'sign out', onClick:()=>{
+        localStorage.clear()
+            window.location.reload()
+        } },
 ]
 
 export const DropdownImageTrigger = () => (
     <Dropdown trigger={trigger} options={options} pointing='top left' icon={null} />
 )
-
-// export default DropdownImageTriggerExample
 const NavBar = (props) =>{
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+
             <div className="container">
                 <Link className="navbar-brand" to="/">Fasts Foods Fast</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-default"
@@ -50,32 +52,13 @@ const NavBar = (props) =>{
                     <ul className="navbar-nav ml-lg-auto">
                         {!props.IsAuthenticated ?
                             <li className="nav-item  btn btn-white">
-                                Login
+                                <Link to="/login"> Login </Link>
+
                             </li>
                             : <li className="nav-item text-white">
                             <DropdownImageTrigger/>
                         </li>}
 
-                        <li className="nav-item">
-                            <a className="nav-link nav-link-icon" href="#">
-                                <i className="ni ni-notification-70"></i>
-                                <span className="nav-link-inner--text d-lg-none">Profile</span>
-                            </a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link nav-link-icon" href="#" id="navbar-default_dropdown_1" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i className="ni ni-settings-gear-65"></i>
-                                <span className="nav-link-inner--text d-lg-none">Settings</span>
-                            </a>
-                            <div className="dropdown-menu dropdown-menu-right"
-                                 aria-labelledby="navbar-default_dropdown_1">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </li>
                     </ul>
 
                 </div>
